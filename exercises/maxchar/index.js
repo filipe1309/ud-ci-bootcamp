bb3615ff82e8cc2bff1e435a328a28fab471312b
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-15 15:54:05 -0300

    feat(section-06): add my solution to max chars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  return mySolution1(str);
+}
+
+function mySolution1(str) {
+  const obj = {};
+  let higherCount = 0;
+  let higherChar = '';
+
+  for (const char of str) {
+    if (obj[char] == undefined) {
+      obj[char] = 0;
+    }
+    obj[char]++;
+    if (obj[char] >= higherCount) {
+      higherCount = obj[char];
+      higherChar = char;
+    }
+  }
+
+  return higherChar;
+}
 
 module.exports = maxChar;
