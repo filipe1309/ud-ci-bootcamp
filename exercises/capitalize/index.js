5351003f354c4d040bf77ad84a7649fca07cec25
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-19 22:48:50 -0300

    feat(section-10): add solution 2 to capitalization challenge

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -22,4 +22,13 @@ function mySolution(str) {
   return wordsArr.join(" ");
 }
 
+function solution2(str) {
+  let result = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i-1] === " ") result += str[i].toUpperCase();
+    else result += str[i];
+  }
+  return result;
+}
+
 module.exports = capitalize;
