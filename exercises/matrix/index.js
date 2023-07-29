074c7eb2cce595e264346d9166834f5544cb30c4
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-29 20:10:47 -0300

    feat(section-14): add solution to spiral matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,47 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  return mySolution1(n);
+}
+
+function mySolution1(n) {
+  const results = Array(n).fill().map(()=>Array(n).fill());
+  // const results = [];
+  // for (let i = 0; i < n; i++) { results.push([]); }
+  let startColumn = 0;
+  let startRow = 0;
+  let endColumn = n - 1;
+  let endRow = n - 1;
+  let counter = 1;
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // Start row
+    for (let col = startColumn; col <= endColumn; col++) {
+      results[startRow][col] = counter++;
+    }
+    startRow++;
+
+    // End Column
+    for (let row = startRow; row <= endRow; row++) {
+      results[row][endColumn] = counter++;
+    }
+    endColumn--;
+
+    // End Row
+    for (let col = endColumn; col >= startColumn; col--) {
+      results[endRow][col] = counter++;
+    }
+    endRow--;
+
+    // Star Column
+    for (let row = endRow; row >= startRow; row--) {
+      results[row][startColumn] = counter++;
+    }
+    startColumn++;
+  }
+
+  return results;
+}
 
 module.exports = matrix;
