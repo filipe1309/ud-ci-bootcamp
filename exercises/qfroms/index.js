21b365236be4b02ad33bbb69f5ecd034d319718a
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-03 21:17:12 -0300

    feat(section-20): add my solution to qfroms

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,36 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stack1 = new Stack();  
+    this.stack2 = new Stack(); 
+  }
+
+  add(record) {
+    // move out items from stack1 to stack2
+    this.#moveItemsBetweenStacks(this.stack1, this.stack2);
+
+    // insert new record on stack1
+    this.stack1.push(record);
+
+    // move back items from stack2 to stack1
+    this.#moveItemsBetweenStacks(this.stack2, this.stack1);
+  }
+
+  #moveItemsBetweenStacks(stackFrom, stackTo) {
+    while (stackFrom.peek()) {
+      stackTo.push(stackFrom.pop());
+    }
+  }
+
+  remove() {
+    return this.stack1.pop()
+  }
+
+  peek() {
+    return this.stack1.peek()
+  }
+}
 
 module.exports = Queue;
