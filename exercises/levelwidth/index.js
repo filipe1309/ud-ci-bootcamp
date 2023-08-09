2d226529509c7ad5ffbd4968c9221ce4703206ff
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-09 17:47:46 -0300

    feat(section-26): add level width solution

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -5,12 +5,29 @@
 // --- Example
 // Given:
 //     0
-//   / |  \
+//   / | \
 // 1   2   3
 // |       |
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let result = [1];
+  let children = root.children;
+  let arr = [];
+
+  do {
+    arr = [];
+    let levelCounter = 0;
+    for (let i = 0; i < children.length; i++) {
+      levelCounter++;
+      arr.push(...children[i].children);
+    }
+    children = arr;
+    result.push(levelCounter);
+  } while (arr.length);
+
+  return result;
+}
 
 module.exports = levelWidth;
