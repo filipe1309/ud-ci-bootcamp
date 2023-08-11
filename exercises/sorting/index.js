20f89db4e3565b394d3cd72961477fd8994c018d
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-11 19:18:14 -0300

    feat(section-32): add solution to SelectionSort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -10,9 +10,7 @@ function swap(arr, i, j) {
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - i; j++) {
-      if (arr[j] > arr[j+1]) {
-        swap(arr, j, j+1);
-      }
+      if (arr[j] > arr[j+1]) swap(arr, j, j+1);
     }
   }
 
@@ -20,7 +18,15 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let indexOfMin = i;
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[j] < arr[indexOfMin]) indexOfMin = j;
+    }
+    if (indexOfMin !== i) swap(arr, i, indexOfMin);
+  }
 
+  return arr;
 }
 
 function mergeSort(arr) {
