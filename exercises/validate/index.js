aa374ea5ce02606706a4f2186bc1df68a6f9d4e9
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-10 21:20:23 -0300

    feat(section-28): add solution to validate bst

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -6,11 +6,25 @@
 // the parent
 
 function validate(node, min = null, max = null) {
+  return solution(node, min, max);
+}
+
+function mySolution(node, min = null, max = null) {
   if (!node) return true;
   if (min && node.data < min) return false;
   if (max && node.data > max) return false;
   
-  return validate(node.left, min, node.data) && validate(node.right, node.data, max);
+  return mySolution(node.left, min, node.data) && mySolution(node.right, node.data, max);
+}
+
+function solution(node, min = null, max = null) {
+  if (min && node.data < min) return false;
+  if (max && node.data > max) return false;
+
+  if (node.left && !solution(node.left, min, node.data)) return false;
+  if (node.right && !solution(node.right, node.data, max)) return false;
+
+  return true;
 }
 
 module.exports = validate;
